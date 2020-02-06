3787947ba11492c4ee4dc6895a83b1b589671db1
Alexandra <sunny.belyavskaya@gmail.com>
2020-02-07 02:00:31 +0300

    grid

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header =() =>{
+    return(
+        <header className='header'>
+            <img src="https://i.pinimg.com/280x280_RS/02/55/bd/0255bd37dc94890ac63e24760d772f7b.jpg" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile =() =>{
+    return(
+        <div className='content'>
+            <div>
+                <img src='https://i.ytimg.com/vi/Pq99ssg35oI/maxresdefault.jpg'/>
+            </div>
+            <div>
+                ava
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
