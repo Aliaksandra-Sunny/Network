1a5d886ae4939c404f26353b5dd38926cd734e0c
Alexandra <sunny.belyavskaya@gmail.com>
2020-03-29 00:13:17 +0300

    Updates

---
+++ precache-manifest.7935beba1eba24242f191cecaf6710a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3debab056e176ca5573cf978ef648d9a",
+    "url": "/Network/index.html"
+  },
+  {
+    "revision": "67b9fd90fda4f878c9d6",
+    "url": "/Network/static/css/main.93dc1d60.chunk.css"
+  },
+  {
+    "revision": "3ef96f9d6b391011e9b0",
+    "url": "/Network/static/js/2.4492d6bc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Network/static/js/2.4492d6bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b9fd90fda4f878c9d6",
+    "url": "/Network/static/js/main.187fbc91.chunk.js"
+  },
+  {
+    "revision": "9161fb71f2e56dfb1431",
+    "url": "/Network/static/js/runtime-main.f7c2c522.js"
+  }
+]);
